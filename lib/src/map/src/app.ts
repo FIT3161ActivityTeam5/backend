cf8c06818357e08e4e481a3f261556e777044bbe
Timothy Samraj <tsam0005@student.monash.edu>
2021-09-01 08:14:50 +1000

    Add functionality for GET requests, add sample events

--- lib/src/map/src/app.ts
+++ lib/src/map/src/app.ts
@@ -1,11 +1,85 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import {
+    DynamoDBClient,
+    QueryCommand,
+    QueryCommandInput,
+} from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({});
+
+interface mapData {
+    [mapID: string]: {
+        S: string;
+    };
+    associatedUserID: {
+        S: string;
+    };
+}
+
+interface cleanMapData {
+    [mapID: string]: string;
+    associatedUserID: string;
+}
+
+const cleanMapData = (mapData: mapData) => {
+    let cleanedData: any = {};
+    for (var key in mapData) {
+        let obj = mapData[key];
+        cleanedData[key] = obj.S;
+    }
+    return cleanedData;
+};
 
 const handlePost = (event: APIGatewayProxyEventV2): APIGatewayProxyResultV2 => {
-    return { statusCode: 200, body: "Hello from post" };
+    return { statusCode: 200, body: "Hello from postt" };
 };
-const handleGet = (event: APIGatewayProxyEventV2): APIGatewayProxyResultV2 => {
-    return { statusCode: 200, body: "Hello from get" };
+
+const handleGet = async (
+    event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyResultV2> => {
+    // Extracting the map ID from the GET request
+    const params = event.pathParameters;
+    if (!params) {
+        return { statusCode: 400, body: "No map ID" };
+    }
+    const mapID = params.mapid;
+    if (!mapID) {
+        return { statusCode: 400, body: "No map ID" };
+    }
+
+    // Setting up the DynamoDB query
+    const queryParams: QueryCommandInput = {
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: "mapID = :s",
+        ExpressionAttributeValues: {
+            ":s": {
+                S: mapID,
+            },
+        },
+    };
+    const queryCommand = new QueryCommand(queryParams);
+
+    // Run the query
+    try {
+        const data = await client.send(queryCommand);
+        if (!data.Items) {
+            throw "No items found";
+        }
+        const res: mapData | {} = cleanMapData(data.Items[0] as mapData);
+        if (Object.keys(res).length === 0) {
+            return { statusCode: 404, body: "Map not found" };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(res),
+        };
+    } catch (error) {
+        console.log(error);
+    }
+
+    return { statusCode: 500, body: "Internal Server Error" };
 };
+
 const handlePatch = (
     event: APIGatewayProxyEventV2
 ): APIGatewayProxyResultV2 => {
@@ -15,7 +89,7 @@ const handlePatch = (
 export const handler = async (
     event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResultV2> => {
-    console.log(event.requestContext.authorizer);
+    console.log(event);
 
     const requestType = event.requestContext.http.method;
     let result: APIGatewayProxyResultV2;
@@ -25,7 +99,7 @@ export const handler = async (
             result = handlePost(event);
             break;
         case "GET":
-            result = handleGet(event);
+            result = await handleGet(event);
             break;
         case "PATCH":
             result = handlePatch(event);
@@ -37,9 +111,4 @@ export const handler = async (
     }
 
     return result;
-    // const queries = JSON.stringify(event.queryStringParameters);
-    // return {
-    //     statusCode: 200,
-    //     body: `Queries: ${queries}`,
-    // };
 };
